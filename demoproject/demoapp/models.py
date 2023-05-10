4853b118c0aa01feb47a403e05fd037810261fd0
Magesh <119650512+bad3767@users.noreply.github.com>
2023-05-10 16:11:23 +0530

    Add files via upload

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    task = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
+    Name = models.CharField(max_length=30)
+    Designation = models.CharField(max_length=100)
+    Date_Of_Birth = models.DateField()
