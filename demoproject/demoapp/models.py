094d5ea0eea1f5fa3a28798082bd8dc4cb94d254
Magesh <119650512+bad3767@users.noreply.github.com>
2023-05-10 16:13:36 +0530

    Delete demoproject directory

--- demoproject/demoapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-    Name = models.CharField(max_length=30)
-    Designation = models.CharField(max_length=100)
-    Date_Of_Birth = models.DateField()
